a18c608d5f67828db3927ad335541e63a00822db
DevGabriel0402 <gabriel.profissional01@gmail.com>
2025-05-17 21:39:11 -0300

    Corrigindo a 'Logo'

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Container } from "@/styles/Styles";
-import Logo from "@/assets/logo.png";
+import Logo from "@/assets/Logo.png";
 import { Carrossel } from "@/components/Carrossel";
 
 // Importando as listas separadas por gênero
